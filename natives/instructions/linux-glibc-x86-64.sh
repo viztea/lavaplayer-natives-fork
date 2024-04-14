d97346c6c2d062c7e2633344affa5f458d449b21
Devoxin <lukel9914@gmail.com>
2024-04-14 15:59:38 +0100

    update instructions (again)

--- natives/instructions/linux-glibc-x86-64.sh
+++ natives/instructions/linux-glibc-x86-64.sh
@@ -6,13 +6,11 @@ apt install -y git cmake build-essential autoconf autogen libtool openjdk-11-jdk
 wget http://ftp.de.debian.org/debian/pool/main/a/automake-1.16/automake_1.16.5-1.3_all.deb
 dpkg -i automake_1.16.5-1.3_all.deb
 
-git clone https://github.com/lavalink-devs/lavaplayer
-git checkout patch/bump-natives
-cd lavaplayer
+git clone https://github.com/devoxin/lavaplayer-natives-fork
+cd lavaplayer-natives-fork
 chmod +x ./gradlew
-cd natives
 
-../gradlew load
+./gradlew load
 
-touch opus/opus-1.5.1/aclocal.m4 configure
-../gradlew compileNatives
+touch natives/opus/opus-1.5.1/aclocal.m4 configure
+./gradlew compileNatives
