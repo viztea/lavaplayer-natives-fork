862efb59bc291ee331353758366df73132f7892b
Devoxin <lukel9914@gmail.com>
2024-04-14 15:59:00 +0100

    update instructions

--- natives/instructions/linux-glibc-x86-64.sh
+++ natives/instructions/linux-glibc-x86-64.sh
@@ -1,10 +1,10 @@
 docker run --rm -it ubuntu:18.04 bash
 
 apt update && apt upgrade
-apt install -y git cmake build-essential autoconf openjdk-17-jdk wget nano libsamplerate-dev
+apt install -y git cmake build-essential autoconf autogen libtool openjdk-11-jdk wget nano libsamplerate-dev
 
-wget http://ftp.de.debian.org/debian/pool/main/a/automake-1.16/automake_1.16.1-4_all.deb
-dpkg -i automake_1.16.1-4_all.deb
+wget http://ftp.de.debian.org/debian/pool/main/a/automake-1.16/automake_1.16.5-1.3_all.deb
+dpkg -i automake_1.16.5-1.3_all.deb
 
 git clone https://github.com/lavalink-devs/lavaplayer
 git checkout patch/bump-natives
@@ -13,8 +13,6 @@ chmod +x ./gradlew
 cd natives
 
 ../gradlew load
-
-# comment out PKG_CHECK_MODULES: L
 
 touch opus/opus-1.5.1/aclocal.m4 configure
 ../gradlew compileNatives
